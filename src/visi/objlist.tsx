e7b6ce0c02281323aaafbc818a3f48078e455e83
Andrew Plotkin <zarf@ZarfLent.local>
2025-12-21 20:04:14 -0500

    List the objects.

--- src/visi/objlist.tsx
+++ src/visi/objlist.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useState, useContext, createContext } from 'react';
 
-import { gamedat_attribute_nums } from '../custom/gamedat';
+import { gamedat_attribute_nums, gamedat_object_ids } from '../custom/gamedat';
 import { ReactCtx } from './context';
+import { ObjPageLink } from './widgets';
 
 export function ObjectAttrList({ attr } : { attr:number })
 {
@@ -14,6 +15,26 @@ export function ObjectAttrList({ attr } : { attr:number })
         return <div>Attribute { attr } not found</div>;
     }
 
+    let objls = [];
+    
+    let counter = 0;
+    for (let tup of zstate.objects) {
+        let obj = gamedat_object_ids.get(tup.onum);
+        let curflag = (tup.attrs & (1 << (31-attr)));
+        if (curflag && obj) {
+            objls.push(
+                <span key={ obj.onum }>
+                    {counter++ ? ', ' : ' '}
+                    { (rctx.shownumbers ?
+                       <span className="ShowAddr">({ obj.onum }) </span>
+                       : null) }
+                    <ObjPageLink onum={ obj.onum } />
+                    <code>{ obj.name }</code>
+                </span>
+            )
+        }
+    }
+    
     function evhan_click_back(ev: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
         ev.preventDefault();
         rctx.setTab('objtree');
@@ -25,6 +46,11 @@ export function ObjectAttrList({ attr } : { attr:number })
                 <a href="#" onClick={ evhan_click_back }>Back to World</a>
             </div>
             <div>Objects with attribute <code>{ attrdat.name }</code>:</div>
+            { (objls.length ?
+               <div>
+                   { objls }
+               </div>
+               : null) }
         </div>
     )
 }
