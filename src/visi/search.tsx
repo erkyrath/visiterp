37a99b2da31dd9a145bcea4d600d359e02861e45
Andrew Plotkin <zarf@ZarfLent.local>
2026-08-05 22:30:54 -0400

    Clicking, and display tweaks.

--- src/visi/search.tsx
+++ src/visi/search.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
+
+import { find_sourceloc_for_id } from './gamedat';
+
+import { ReactCtx } from './context';
 
 export function SearchField()
 {
@@ -29,11 +33,20 @@ export function SearchField()
 
 export function SearchResults()
 {
+    let rctx = useContext(ReactCtx);
+    
+    function evhan_click(idtype: string, id: string) {
+        let loc = find_sourceloc_for_id(idtype, id);
+        if (loc) {
+            rctx.setLoc(loc, (idtype == 'GLOB'));
+        }
+    }
+    
     return (
         <div className="SearchResults">
             <ul>
-                <li className="SearchResultItem">obj LANTERN</li>
-                <li className="SearchResultItem">rtn LANTERN-F</li>
+                <li className="SearchResultItem" onClick={ ()=>evhan_click('OBJ', 'SWORD') }>obj <code>SWORD</code></li>
+                <li className="SearchResultItem" onClick={ ()=>evhan_click('RTN', 'LANTERN') }>rtn <code>LANTERN</code></li>
             </ul>
         </div>
     );
