880beb7bce465f9058a7eadb4e022019cc54c6f1
Andrew Plotkin <zarf@ZarfLent.local>
2026-08-05 21:25:33 -0400

    Events.

--- src/visi/search.tsx
+++ src/visi/search.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useRef } from 'react';
 
 export function SearchField()
 {
+    let inputref = useRefInput();
+
+    function evhan_focus() {
+        console.log('### focus');
+    }
+    
+    function evhan_blur() {
+        console.log('### blur');
+    }
+    
+    function evhan_change(ev: ChangeEv) {
+        if (inputref.current) {
+            console.log('### change', inputref.current.value);
+        }
+    }
+    
     return (
         <div className="SearchBox">
             <div className="SearchIcon">&#x26B2;</div>
-            <input className="SearchField" type="search" />
+            <input className="SearchField" type="search" ref={ inputref } onChange={ evhan_change } onFocus={ evhan_focus } onBlur={ evhan_blur } />
         </div>
     );
 }
+
+type ChangeEv = React.ChangeEvent<HTMLInputElement>;
+const useRefInput = () => useRef<HTMLInputElement>(null);
