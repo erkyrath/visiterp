553a82e2a6a3add318e861f0ef331a085e482737
Andrew Plotkin <zarf@ZarfLent.local>
2026-08-07 20:49:21 -0400

    Clean up some stuff.

--- src/visi/search.ts
+++ src/visi/search.ts
@@ -3,18 +3,19 @@ import { ObjectData, RoutineData } from './gametypes';
 export type ResultItem = {
     idtype: string;
     label: string;
+    exact?: boolean;
     sourceloc: string;
 };
 
 let current_term: string = '';
 let results: ResultItem[] = [];
-let event_id = 0;
+let timer_id = 0;
 
 const MAX_RESULTS = 20;
 
 export function set_search_term(val: string)
 {
-    val = val.toUpperCase();
+    val = val.toUpperCase().trim();
     if (val == current_term) {
         return;
     }
@@ -22,20 +23,20 @@ export function set_search_term(val: string)
     current_term = val;
     results = [];
 
-    if (event_id) {
-        clearInterval(event_id);
-        event_id = 0;
+    if (timer_id) {
+        clearInterval(timer_id);
+        timer_id = 0;
     }
 
-    event_id = setInterval(search_worker, 500) as unknown as number;
+    timer_id = setInterval(search_worker, 500) as unknown as number;
 }
 
 function search_worker()
 {
     console.log('### search_worker');
     if (current_term == '') {
-        clearInterval(event_id);
-        event_id = 0;
+        clearInterval(timer_id);
+        timer_id = 0;
         return;
     }
 
@@ -63,8 +64,8 @@ function search_worker()
         }
     }
 
-    clearInterval(event_id);
-    event_id = 0;
+    clearInterval(timer_id);
+    timer_id = 0;
     
     console.log('### dispatching', results.length);
     window.dispatchEvent(new CustomEvent('search-results', { detail:[ ...results ] }));
